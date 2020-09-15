7adfb9a8233279b137cd3d1a181cca421f921417
Emerson Delatorre <emerson@delatorre.dev>
2020-09-15 16:48:05 -0300

    Primeira alteração na controller

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,14 +17,15 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVA App from C#";
+            ViewData["e-mail"] = "emerson@delatorre.dev";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "(22) 99999-9999.";
 
             return View();
         }
